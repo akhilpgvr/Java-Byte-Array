d9ca8cbc1c5c087ae568dd1265a3f5cfc9158b69
akhilpgvr <akhilpgvr@gmail.com>
2023-01-03 12:10:37 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.nio.charset.StandardCharsets;
+
+public class Test{
+    public static void main(String[] args){
+        String name = "java bytes";
+        byte[] nameAsByte = name.getBytes();
+        System.out.println("values stored in Byte [] is : ");
+        for(byte i : nameAsByte){
+            System.out.println(i);
+        }
+        // System.out.prinln("Coverted value from byte [] to String : ");
+        System.out.println(new String(nameAsByte,StandardCharsets.UTF_8));
+    }
+}
